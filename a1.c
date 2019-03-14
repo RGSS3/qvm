45a79ffac4904b89201380310dcbd16ea26e509a
you <you>
2019-03-14 16:31:01 +0800

    init

--- a1.c
+++ a1.c
@@ -1,15 +1,17 @@
 #include "./code.h"
 
 
-void interpret(ins_t code[]) {
 
+void interpret(ins_t code[]) {
+  int stack[64], top = 0;
+  int mem[64] = {};
 }
 
 ins_t code[] = {
-    {READ, 0},
-    {READ, 1},
-    {PUSH, 0},
-    {PUSH, 1},
+    {READ, {0}},
+    {READ, {1}},
+    {PUSH, {0}},
+    {PUSH, {1}},
     {ADD},
     {WRITE},
     {HALT}
